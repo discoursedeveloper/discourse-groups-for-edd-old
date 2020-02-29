5c7b75e8cae0e7651601dbd1e6f83732b3de6c51
Discourse Developer <discoursedeveloper@gmail.com>
2020-02-29 19:30:39 +0530

    added main file

---
+++ discourse-groups-for-edd.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin's main file
+ *
+ * @package DiscourseEDD
+ */
+
+/*
+Plugin Name: Discourse Groups For EDD
+Plugin URI: https://github.com/discoursedeveloper/
+Description: Integrate Discourse forums with EDD Plugin and EDD Subscription extensions.
+Version: 1.0.0
+Author: Discourse Developer
+Author URI: https://github.com/discoursedeveloper/
+License: GPLv2 or later
+Text Domain: dis_groups_edd
+*/
