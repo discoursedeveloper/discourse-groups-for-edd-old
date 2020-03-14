6360759178a99af7b03be5996a1749fd61fcfe5c
Discourse Developer <discoursedeveloper@gmail.com>
2020-03-14 13:26:47 +0530

    Added blank class for settings related code

---
+++ admin/disco-edd-admin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * File for class wrote for admin dashboard functionality related code
+ *
+ * @package DiscourseEDD
+ */
+
+namespace DiscoEdd\Admin;
+
+/**
+ * Class for code to process add/update/remove users to Discourse Groups
+ */
+class DiscoEddAdmin {
+
+}
+new DiscoEddAdmin();
